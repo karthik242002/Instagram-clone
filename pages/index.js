052c22ed7fadd8cc313c676b3954a741b242aa83
karthik242002 <karthiksiva153@gmail.com>
2023-01-31 19:45:44 +0530

    install next js and Tailwind CSS and create the first template

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello World</h1>
+
+      {/* Header */}
+
+
+      {/* Feed */}
+
+
+      {/* Modal */}
     </div>
   )
 }
