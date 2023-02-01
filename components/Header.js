fb56d00f821be19a413f67bd0ac5ac4065f7ecf8
karthik242002 <karthiksiva153@gmail.com>
2023-02-02 00:00:45 +0530

    updated header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-
-=======
-import {Image} from 'next/image'
->>>>>>> 6776be62c63afde3c4a0d9a3152c66bfcacf162f
 import React from 'react'
-import { FaSearchIcon } from "react-icons/fa";
+import { SearchIcons } from "@heroicons/react/outline";
 import Image from 'next/image';
 
 export default function Header() {
@@ -15,20 +10,20 @@ export default function Header() {
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
                     layout='fill'
-                    className='object-contain' />
+                    className="object-contain" />
 
             </div>
             <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
-                    layout='fill'
-                    className='object-contain' />
+                    layout="fill"
+                    className="object-contain" />
 
             </div>
             { /* Middle */}
             <div className=" ">
                 <div className="">
-                    <FaSearchIcon className="h-5" />
+                    <SearchIcons className="h-5" />
 
                     { /* Right */}
                     <h1>Right sides</h1>
