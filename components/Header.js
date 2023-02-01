d14104c367fe4bdbe77ff01882cc9d9a35ce8295
karthik242002 <karthiksiva153@gmail.com>
2023-02-01 23:02:32 +0530

    updated header

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,20 @@
-import Image from 'next/image'
+
 import React from 'react'
 import { FaSearchIcon } from "react-icons/fa";
+import Image from 'next/image';
 
 export default function Header() {
     return (
         <div className="flex items-center justify-between max-w-6xl">
             { /* Left */}
-            <div className="cur h-24 w-24 relative hidden lg:inline-grid">
+            <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
                     layout='fill'
                     className='object-contain' />
 
             </div>
-            <div className="cur h-24 w-10 relative  lg:hidden">
+            <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
                     layout='fill'
@@ -23,13 +24,13 @@ export default function Header() {
             { /* Middle */}
             <div className=" ">
                 <div className="">
-                    <FaSearchIcon />
+                    <FaSearchIcon className="h-5" />
 
                     { /* Right */}
                     <h1>Right sides</h1>
                 </div>
             </div>
-         </div>
-                )
+        </div>
+    )
 
 }
