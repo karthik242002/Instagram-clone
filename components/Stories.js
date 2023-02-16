3975c8cc0480366c9bc69cd4ee8746b1d9ba5d2b
karthik242002 <karthiksiva153@gmail.com>
2023-02-16 20:03:06 +0530

    updated stories

--- components/Stories.js
+++ components/Stories.js
@@ -17,6 +17,7 @@ export default function Stories() {
         setStoryUsers(storyUsers);
         console.log(storyUsers);
     }, []);
+
     return <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none'>
         {storyUsers.map(user => (
             <Story key={user.id} username={user.username} img={user.img} />
