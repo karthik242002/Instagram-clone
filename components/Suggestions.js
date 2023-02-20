15b57e6de3f76a56f4b91e1393820093aa0be6f9
karthik242002 <karthiksiva153@gmail.com>
2023-02-20 22:47:05 +0530

    updated suggestions

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -39,6 +39,7 @@ export default function Suggestions() {
                     <button className="font-semibold text-blue-400 text-sm">
                         Follow
                     </button>
+                    
                 </div>
             ))}
 
